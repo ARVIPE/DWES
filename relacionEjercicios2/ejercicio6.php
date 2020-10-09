45bbe81cc4f4ac6da63ed8a3faaf78b76a000648
ARVIPE <arvipe@hotmail.com>
2020-10-09 09:14:32 +0200

    actualización

---
+++ relacionEjercicios2/ejercicio6.php
@@ -0,0 +1,12 @@
+<?php
+
+$array = array("Madrid", "Barcelona", "Londres", "New York", "Los Ángeles", "Chicago");
+
+foreach($array as $valor => $nombre){
+    echo "La ciudad con el indice $valor tiene el nombre de $nombre <br>";
+}
+
+
+
+
+?>
