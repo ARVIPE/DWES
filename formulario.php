06f09bb9083a4a39bce92fea3a59b1973f179863
ARVIPE <arvipe@hotmail.com>
2020-10-13 20:10:49 +0200

    actualización

--- formulario.php
+++ formulario.php
@@ -2,41 +2,62 @@
 http://www.w3.org/TR/html4/loose.dtd">
 <!-- Desarrollo Web en Entorno Servidor -->
 <!-- Tema 2 : Características del Lenguaje PHP -->
-<!-- Ejemplo: Procesar datos en la misma página que el formulario -->
+<!-- Ejemplo: Validar datos en la misma página que el formulario -->
 <html>
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<title>Desarrollo Web</title>
-</head>
-<body>
+     <head>
+          <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+          <title>Desarrollo Web</title>
+     </head>
+     <body>
 <?php
-if (isset($_POST['enviar'])) {
-$nombre = $_POST['nombre'];
-$modulos = $_POST['modulos'];
-print "Nombre: ".$nombre."<br />";
-foreach ($modulos as $modulo) { 
-print "Modulo: ".$modulo."<br />";
-}
-} else {
+     if (!empty($_POST['modulos']) && !empty($_POST['nombre'])) {
+          $nombre = $_POST['nombre'];
+          $modulos = $_POST['modulos'];
+          print "Nombre: ".$nombre."<br />";
+          foreach ($modulos as $modulo) {
+               print "Modulo: ".$modulo."<br />";
+          }
+     }
+     else {
 ?>
-<form name="input" action="<?php echo $_SERVER['PHP_SELF'];?>" method="post">
-Nombre del alumno: <input type="text" name="nombre" /><br />
-<p>Módulos que cursa:</p>
-<input type="checkbox" name="modulos[]" value="DWES" />
-Desarrollo web en entorno servidor<br />
-<input type="checkbox" name="modulos[]" value="DWEC" />
-Desarrollo web en entorno cliente<br />
-<br />
-<input type="submit" value="Enviar" name="enviar"/>
-</form>
+     <form name="input" action="<?php echo $_SERVER['PHP_SELF'];?>" method="post">
+          Nombre del alumno: <input type="text" name="nombre" value="<?php
+          
+               if(!empty($_POST['nombre'])){
+                  echo $_POST['nombre'];
+               }
+               ?>"/>
+          <?php
+               if (isset($_POST['enviar']) && empty($_POST['nombre']))
+                    echo "<span style='color:red'> &lt;-- Debe introducir un nombre!!</span>"
+          ?>
+          <br />
+          <p>Módulos que cursa:
+          <?php
+               if (isset($_POST['enviar']) && empty($_POST['modulos']))
+                    echo "<span style='color:red'> &lt;-- Debe escoger al menos uno!!</span>"
+          ?>
+          </p>
+          <input type="checkbox" name="modulos[]" value="DWES"
+               <?php
+                    if(isset($_POST['modulos']) && in_array("DWES",$_POST['modulos']))
+                         echo 'checked="checked"';
+               ?>
+          />
+               Desarrollo web en entorno servidor
+               <br />
+          <input type="checkbox" name="modulos[]" value="DWEC"
+               <?php
+                    if(isset($_POST['modulos']) && in_array("DWEC",$_POST['modulos']))
+                         echo 'checked="checked"';
+               ?>
+          />
+               Desarrollo web en entorno cliente<br />
+          <br />
+          <input type="submit" value="Enviar" name="enviar"/>
+     </form>
 <?php
-}
+     }
 ?>
-<?php
-if (!empty($_POST['modulos']) && !empty($_POST['nombre']))
-echo "Los datos son correctos";
-if (empty($_POST['nombre'])) 
-  $nombre = "Este campo no puede estar vacío";
-?>
-</body>
-</html> 
+     </body>
+</html>
