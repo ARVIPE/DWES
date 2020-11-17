ee06698ca23f546bcac4b1ad68e0bc6d2c87794f
ARVIPE <55533930+ARVIPE@users.noreply.github.com>
2020-11-17 21:47:17 +0100

    actualización

--- cookie/formularioAcceso/index.php
+++ cookie/formularioAcceso/index.php
@@ -3,12 +3,31 @@
         <title>title</title>
     </head>
     <body>
+
+        <form action="prueba-cookies.php" method="post">
+            Usuario: <input type="text" name="usuario">
+            <br>
+            Clave: <input type="text" name="clave">
+            <br>
+            <input type="checkbox" name="guardar_clave" value="1">Recuerdame
+            <br>
+            <input type="hidden" name="cookiecreada" value"<?php $_COOKIE['nombre'] ?>">
+            <br>
+            <input type="submit" name="entrar" value="entrar">
+        </form>
         
-        Usuario: <input type="text" name="usuario" value=""><br><br>
-        Contraseña: <input type="text" name="contraseña" value=""><br><br>
-        Recordarme: <input type="checkbox" name="recordar" value=""><br><br>
-        <input type="submit" name="acceder" value="acceder"><br><br>
+        <?php
+        
+        if(isset($_POST["entrar"]) && isset($_POST["guardar_clave"])){
+            setcookie("nombre", $_POST['usuario']);
+            setcookie("clave", $_POST['clave']);
+            echo $_COOKIE["nombre"]."<br>";
+            echo $_COOKIE["clave"];
+            setcookie('ultimoAcceso', date('d-m-y h:i:s'), time()+3600);     
+        }
         
         
+        ?>
+
     </body>
 </html>
---
+++ cookie/formularioAcceso/prueba-cookies.php
@@ -0,0 +1,18 @@
+<html>
+    <head>
+        <title>acceso correcto</title>
+    </head>
+    <body>
+
+    <?php
+    
+    echo $_POST["cookiecreada"];
+    
+    "Hola".$_POST["cookiecreada"]."su última visita fue el".$_COOKIE['ultimoAcceso']; 
+    
+    
+    ?>
+
+
+    </body>
+</html>
