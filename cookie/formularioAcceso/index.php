650c629996390b33cdbd91db7c614c8328499103
ARVIPE <arvipe@hotmail.com>
2020-11-17 10:55:11 +0100

    actualización

---
+++ cookie/formularioAcceso/index.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+        <title>title</title>
+    </head>
+    <body>
+        
+        Usuario: <input type="text" name="usuario" value=""><br><br>
+        Contraseña: <input type="text" name="contraseña" value=""><br><br>
+        Recordarme: <input type="checkbox" name="recordar" value=""><br><br>
+        <input type="submit" name="acceder" value="acceder"><br><br>
+        
+        
+    </body>
+</html>
