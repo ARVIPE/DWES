085103a72c37a029fcaf0f20fab32021f79d591b
ARVIPE <55533930+ARVIPE@users.noreply.github.com>
2020-11-19 18:41:36 +0100

    Revert "Revert "Revert "actualización"""
    This reverts commit 17a1b3e5aa95cb57b13eb7da35e09fa03bcc1a12.

--- cookie/formularioAcceso/index.php
+++ cookie/formularioAcceso/index.php
@@ -13,6 +13,16 @@
         $error = $conex->errorInfo();
         echo $error[2];
 
+        if (isset($_POST['entrar']) && (!empty($_POST['usuario'])) && (!empty($_POST['clave']))) {
+            $result = $conex->query("SELECT * from usuarios");
+            $obj = $result->fetch();
+            if($_POST['usuario'] == $obj['usuario'] && md5($_POST['clave']) === $obj['contraseña']){
+                echo "Correcto";
+            }else{
+            echo "Incrorrecto";
+            }
+        }
+
 
         } catch (PDOException $exc) {
 
@@ -20,24 +30,9 @@
         echo "Error: " . $exc->getMessage(); //error del servidor de la bbdd
         }
         ?>
-        
-        <?php
-         if (isset($_POST['entrar']) && (!empty($_POST['usuario'])) && (!empty($_POST['clave']))) {
-            $result = $conex->query("SELECT * from usuarios");
-            $obj = $result->fetch();
-            if($_POST['usuario'] == $obj['usuario'] && md5($_POST['clave']) === $obj['contraseña']){
-                header('Location: prueba-cookies.php');
-            }else{
-            header('Location: index.php');
-            }
-        } else {
-  
-        
-        
-        ?>
 
 
-        <form action="" method="post">
+        <form action="prueba-cookies.php" method="post">
             Usuario: <input type="text" name="usuario" value="<?php if(isset($_POST["volver"]))echo $_COOKIE["nombre"]; ?>">
             <br>
             Clave: <input type="password" name="clave" value="<?php if(isset($_POST["volver"]))echo $_COOKIE["clave"]; ?>">
@@ -46,10 +41,6 @@
             <input type="submit" name="entrar" value="entrar">
         </form> 
 
-        <?php
-        
-        }
-        ?>
 
 
     </body>
